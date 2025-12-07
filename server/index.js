01fb5154590fe3cbf3ca3e705e8698541e0bd5f9
TWFILM <teachapat555@gmail.com>
2025-12-08 06:07:58 +0700

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -9,9 +9,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Database Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
 // Test Route
 app.get('/', (req, res) => {
-  res.send('Hello from Help Desk API!');
+  res.send('API is running...');
 });
 
 // Start Server
